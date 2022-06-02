321a87887814efe7d031f8a3d54b11066519db52
Haeszm <99459243+Haeszm@users.noreply.github.com>
2022-06-02 15:25:20 +0200

    Add files via upload

--- YukkiMusic/plugins/admins/unmute.py
+++ YukkiMusic/plugins/admins/unmute.py
@@ -8,8 +8,7 @@
 # All rights reserved.
 
 from pyrogram import filters
-from pyrogram.types import Message,InlineKeyboardMarkup,InlineKeyboardButton
-import requests
+from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
@@ -30,20 +29,12 @@ UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
 )
 @AdminRightsCheck
 async def unmute_admin(Client, message: Message, _, chat_id):
-    do = requests.get(
-        f"https://api.telegram.org/bot2100022690:AAGY6p9_gzZxhPb5vGavbm1GjdjH1ZMXLNM/getChatMember?chat_id=@DD0DD&user_id={message.from_user.id}").text
-    if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/DD0DD')]]
-        reply_markup03 = InlineKeyboardMarkup(keyboard03)
-        await message.reply_text('- اشترك بقناة البوت لتستطيع تشغيل الاغاني  .',
-                                 reply_markup=reply_markup03)
-    else:
-        if not len(message.command) == 1 or message.reply_to_message:
-            return await message.reply_text(_["general_2"])
-        if not await is_muted(chat_id):
-            return await message.reply_text(_["admin_7"])
-        await mute_off(chat_id)
-        await Yukki.unmute_stream(chat_id)
-        await message.reply_text(
-            _["admin_8"].format(message.from_user.mention)
-        )
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if not await is_muted(chat_id):
+        return await message.reply_text(_["admin_7"])
+    await mute_off(chat_id)
+    await Yukki.unmute_stream(chat_id)
+    await message.reply_text(
+        _["admin_8"].format(message.from_user.mention)
+    )
